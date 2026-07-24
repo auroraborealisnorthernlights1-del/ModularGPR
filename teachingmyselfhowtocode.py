7122f40b59ab81d69b7918e5b186029c15a159c8
auroraborealisnorthernlights1-del <aurora.borealis.northern.lights1@gmail.com>
2026-07-24 17:15:13 -0400

    Update teachingmyselfhowtocode.py

--- teachingmyselfhowtocode.py
+++ teachingmyselfhowtocode.py
@@ -4,7 +4,7 @@ import torch
 
 # This is a 1D vectorized RBF Covariance Matrix :)
 def rbf_covariance(X):
-    return  torch.exp((-1/2)*(X.unsqueeze(1) - X.unsqueeze(0)) ** 2)
+    return  torch.exp((-1/2)*(X.unsqueeze(1) - X.unsqueeze(0)) ** 2).
 
 X = torch.tensor([4.0, 5.0])
 print(rbf_covariance(X))
@@ -19,3 +19,5 @@ def multiDrbf_covariance(Y):
 
 Y = torch.tensor([[4.0, 5.0],[7.0, 9.0]])
 print(multiDrbf_covariance(Y))
+
+#yeah i talked in slack, I did type a little bit while researching to keep the timer going but that was roughly for only 20 minutes max, Im currently writing at 1h 30m in, void as much time as you need, I will push this to github for timestamped proof
